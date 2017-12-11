23c9390b6b77b200a3a68d779bec62726b9ddd30
keisi2017 <34274010+keisi2017@users.noreply.github.com>
2017-12-11 11:02:04 -0500

    modific 22
    modific 22

--- vicidial/getuser.php
+++ vicidial/getuser.php
@@ -7,12 +7,12 @@ $idAgent;
 $idList;
 $accion;
 ### If you have globals turned off uncomment these lines
+if (isset($_GET["accion"]))						{$accion=$_GET["accion"];}
+	elseif (isset($_POST["accion"]))				{$accion=$_POST["accion"];}
 if (isset($_GET["idAgent"]))						{$idAgent=$_GET["idAgent"];}
 	elseif (isset($_POST["idAgent"]))				{$usidAgenter=$_POST["idAgent"];}
 if (isset($_GET["idList"]))						{$idList=$_GET["idList"];}
 	elseif (isset($_POST["idList"]))				{$idList=$_POST["idList"];}
-if (isset($_GET["accion"]))						{$accion=$_GET["accion"];}
-	elseif (isset($_POST["accion"]))				{$accion=$_POST["accion"];}
 
 
 if ($accion =='insertAgents'){
